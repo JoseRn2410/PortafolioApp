d19e43e207807ec0ec2193ee25cfe7001070f9ed
Jose Nunez <josern24@hgmail.com>
2023-03-14 11:20:26 -0400

    Form contact added and navbar scroll link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AboutMeComponent } from './Portafolio/about-me/about-me.component';
+import { SkillsComponent } from './Portafolio/skills/skills.component';
+import { ContactComponent } from './Portafolio/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
